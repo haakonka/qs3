a24454ac6037362045a374bfeb4ba7632f1ba7a4
Håkon <mew2802@gmail.com>
2022-03-29 17:00:49 +0200

    Added some unfinished methods

--- Backend/src/main/java/edu/ntnu/Backend/controller/LoginController.java
+++ Backend/src/main/java/edu/ntnu/Backend/controller/LoginController.java
@@ -1,4 +1,18 @@
 package edu.ntnu.Backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/login")
+@RestController
+@CrossOrigin
 public class LoginController {
+
+    @GetMapping("/")
+    public void checkIfUserIsValid() {
+
+    }
+
 }
