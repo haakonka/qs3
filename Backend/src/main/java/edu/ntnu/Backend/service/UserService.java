4174a852cd2f41c970462d413a62292574a9c090
Håkon <mew2802@gmail.com>
2022-03-30 10:42:57 +0200

    Added the user login validation method

--- Backend/src/main/java/edu/ntnu/Backend/service/UserService.java
+++ Backend/src/main/java/edu/ntnu/Backend/service/UserService.java
@@ -4,6 +4,9 @@ import edu.ntnu.Backend.repository.UserRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,9 +41,29 @@ public class UserService {
     }
 
     //TODO add dehashing and rehasing
-    public boolean checkIfMatch(UserDAO user) {
-        return true;
-    }
+    public boolean checkIfMatch(String email, String password) {
+        for(int i = 0; i< users.size(); i++) {
+            if(users.get(i).getEmail().contentEquals(email)) {
+                //TODO hmmm what is this
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA-512");
+                    byte[] salt = generateSalt();
+                    md.update(salt);
 
+                    //Password is hashed and ready to be stored.
+                    byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+                    String hash = new String(hashedPassword, StandardCharsets.UTF_8);
+
+                    if(users.get(i).getHash().contentEquals(hash)) {
+                        return true;
+                    }
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
     //Some text to check something
 }
