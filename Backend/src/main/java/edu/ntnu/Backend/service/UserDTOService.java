93629ff4f49b9c9ef24e66accdc424edc8e1ec3c
Håkon <mew2802@gmail.com>
2022-03-30 10:19:51 +0200

    Quick change to UserDTOService

--- Backend/src/main/java/edu/ntnu/Backend/service/UserDTOService.java
+++ Backend/src/main/java/edu/ntnu/Backend/service/UserDTOService.java
@@ -1,6 +1,9 @@
 package edu.ntnu.Backend.service;
 import edu.ntnu.Backend.dto.UserDTO;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 
@@ -9,7 +12,7 @@ public class UserDTOService {
 
     public byte[] generateSalt() {
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[64];
         random.nextBytes(salt);
         return salt;
     }
@@ -17,8 +20,16 @@ public class UserDTOService {
     //TODO add dehashing and rehasing
     public boolean checkIfMatch(UserDTO user) {
         for(int i = 0; i< users.size(); i++) {
-            if(users.get(i).getEmail().contentEquals(user.getEmail()) &&
-            users.get(i).getPassword().contentEquals(user.getPassword())) {
+            if(users.get(i).getEmail().contentEquals(user.getEmail())) {
+                //TODO hmmm what is this
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA-512");
+                    byte[] salt = generateSalt();
+                    md.update(salt);
+                    byte[] hashedPassword = md.digest(user.getPassword().getBytes(StandardCharsets.UTF_8));
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
                 return true;
             }
         }
