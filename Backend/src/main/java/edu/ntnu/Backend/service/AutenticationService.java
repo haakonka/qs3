a7f49c415dd35fb2834a3b83ebe62855f19c64d5
Heeeelyeeee <72669741+Heeeelyeeee@users.noreply.github.com>
2022-03-30 15:34:28 +0200

    added files

---
+++ Backend/src/main/java/edu/ntnu/Backend/service/AutenticationService.java
@@ -0,0 +1,15 @@
+package edu.ntnu.Backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticationService {
+    //make jwt TOKEN with name, role, epost
+
+
+    public AutenticationService() {
+    }
+
+
+
+}
