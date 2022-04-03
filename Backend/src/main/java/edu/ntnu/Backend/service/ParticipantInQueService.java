3a4e734a9ac66bd2b90feaae62fc8341a1df8a0f
Heeeelyeeee <72669741+Heeeelyeeee@users.noreply.github.com>
2022-04-03 18:51:41 +0200

    Update ParticipantInQueService.java
    fiixed sorting in participents in que

--- Backend/src/main/java/edu/ntnu/Backend/service/ParticipantInQueService.java
+++ Backend/src/main/java/edu/ntnu/Backend/service/ParticipantInQueService.java
@@ -18,6 +18,15 @@ public class ParticipantInQueService {
 
     public List<ParticipantInQueDAO> findAllParticipantsInAQue(String subjectCode, int schoolYear) {
         System.out.println("Finding all participants in the que for: " + subjectCode + " at year: " + schoolYear);
+        List<ParticipantInQueDAO> participantInQueDAOS = participantInQueRepository.findAllBySubjectCodeAndSchoolYear(subjectCode, schoolYear);
+        participantInQueDAOS
+                .sort(
+                        (ParticipantInQueDAO p1, ParticipantInQueDAO p2) -> {
+                            if(p1.getJoinedQue().after(p2.getJoinedQue())){
+                                return -1;
+                            }else return 1;
+                        });
+
         return participantInQueRepository.findAllBySubjectCodeAndSchoolYear(subjectCode, schoolYear);
     }
 }
